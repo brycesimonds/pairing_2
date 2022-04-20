e32c818fb64e6e43647c0e90e46a9f42094eb45b
Bryce Simonds <bsimonds14@gmail.com>
2022-04-20 13:29:55 -0600

    Add places

---
+++ friends.rb
@@ -0,0 +1,7 @@
+arrays = ["Carlos", "Sarah", "Tess", "Natalie", "Charles"]
+puts arrays[2]
+puts arrays[4]
+puts arrays[0]
+
+places = ["Rio", "Tokyo", "Hong Kong", "Paris", "Pensacola"]
+puts places.pop
